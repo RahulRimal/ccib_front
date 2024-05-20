8f4ff221b9b394acd68ed9b18874eadc4462dac8
Jeevan Shrestha <shresthaj1986@gmail.com>
2024-05-20 07:25:50 +0545

    Added helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -42,3 +42,32 @@ export function flattenObject(obj, parentKey = "") {
     return acc;
   }, {});
 }
+
+export const usernameConcatenator = (obj) => {
+  return Object.values(obj)
+    .map((value) => value || "")
+    .join(" ");
+};
+
+export const hexWithOpacity = (hex, opacity) => {
+  if (
+    typeof hex !== "string" ||
+    !/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex)
+  ) {
+    throw new Error("Invalid hex color");
+  }
+
+  if (hex.length === 4) {
+    hex = "#" + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
+  }
+
+  if (typeof opacity !== "number" || opacity < 0 || opacity > 100) {
+    throw new Error("Opacity must be a number between 0 and 100");
+  }
+  const alpha = Math.round((opacity / 100) * 255)
+    .toString(16)
+    .padStart(2, "0")
+    .toUpperCase();
+
+  return hex + alpha;
+};
