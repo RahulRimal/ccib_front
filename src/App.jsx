7ff114a8d92b375309c98e39d0d8b55b5c0d3f69
Jeevan Stha <shresthaj1986@gmail.com>
2024-06-18 14:57:02 +0545

    next js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
+import { SnackbarProvider } from "notistack";
 import UserProfile from "./pages/UserProfile";
 import AppBar from "./components/AppBar";
 import Drawer from "./components/Drawer";
 import GlobalStyle, { theme } from "./theme";
 import SignIn from "./pages/SignIn";
-import { useSelector } from "react-redux";
 import LoanForm from "./pages/LoanForm";
 import FinancePage from "./pages/FinancePage";
 import CompanyPage from "./pages/CompanyPage";
@@ -28,6 +22,7 @@ import BaseForm from "./components/Forms/BaseForm";
 import InquiryForm from "./components/Forms/InquiryForm";
 import FormsBasePage from "./pages/base_form/FormsBasePage";
 import UsersPage from "./pages/UsersPage";
+import { store } from "./store";
 
 export const DrawerWidthContext = React.createContext();
 export const drawerMinWidth = 50;
@@ -38,7 +33,6 @@ const App = () => {
   const toggleTheme = () =>
     setThemeMode(themeMode === "light" ? "dark" : "light");
   const [drawerWidth, setDrawerWidth] = React.useState(drawerMaxWidth);
-  const { isLoggedIn } = useSelector((store) => store.auth);
 
   const toggleDrawer = () => {
     setDrawerWidth(
@@ -46,112 +40,65 @@ const App = () => {
     );
   };
 
-  const Layout = () => {
-    return (
-      <>
-        <AppBar
-          drawerWidth={drawerWidth}
-          toggleDrawer={toggleDrawer}
-          toggleTheme={toggleTheme}
-        />
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: `${drawerWidth}px calc(100% - ${drawerWidth}px)`,
-            transition: "all 0.3s ease",
-          }}
-        >
-          <div style={{ maxWidth: drawerWidth }}>
-            <Drawer drawerWidth={drawerWidth} />
-          </div>
-          <div style={{ maxWidth: "100%", margin: "12px", marginBottom: 0 }}>
-            {<Outlet />}
-          </div>
+  const Layout = () => (
+    <>
+      <AppBar
+        drawerWidth={drawerWidth}
+        toggleDrawer={toggleDrawer}
+        toggleTheme={toggleTheme}
+      />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `${drawerWidth}px calc(100% - ${drawerWidth}px)`,
+          transition: "all 0.3s ease",
+        }}
+      >
+        <div style={{ maxWidth: drawerWidth }}>
+          <Drawer drawerWidth={drawerWidth} />
         </div>
-      </>
-    );
-  };
+        <div style={{ maxWidth: "100%", margin: "12px", marginBottom: 0 }}>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: isLoggedIn ? <Layout /> : <SignIn />,
+      element: <Layout />,
       children: [
-        {
-          path: "/",
-          element: <Dashboard />,
-        },
-        {
-          path: "/users",
-          element: <UsersPage />,
-        },
-        {
-          path: "/add",
-          element: <FormsBasePage />,
-          index: true,
-        },
-        {
-          path: "/add/:formKey",
-          element: <FormsBasePage />,
-        },
-
-        {
-          path: "/user-profile",
-          element: <UserProfile />,
-        },
-        {
-          path: "/finance",
-          element: <FinancePage />,
-        },
-        {
-          path: "/company",
-          element: <CompanyPage />,
-        },
-        {
-          path: "/loan-application",
-          element: <LoanApplicationPage />,
-        },
-        {
-          path: "/users/:id",
-          element: <UserProfile />,
-        },
-        {
-          path: "/application",
-          element: <LoanForm />,
-        },
-        {
-          path: "/loans",
-          element: <LoansPage />,
-        },
-        {
-          path: "/report",
-          element: <ReportPage />,
-        },
-        {
-          path: "/profile",
-          element: <EmployeeProfile />,
-        },
+        { path: "/", element: <Dashboard /> },
+        { path: "/users", element: <UsersPage /> },
+        { path: "/add", element: <FormsBasePage />, index: true },
+        { path: "/add/:formKey", element: <FormsBasePage /> },
+        { path: "/user-profile", element: <UserProfile /> },
+        { path: "/finance", element: <FinancePage /> },
+        { path: "/company", element: <CompanyPage /> },
+        { path: "/loan-application", element: <LoanApplicationPage /> },
+        { path: "/users/:id", element: <UserProfile /> },
+        { path: "/application", element: <LoanForm /> },
+        { path: "/loans", element: <LoansPage /> },
+        { path: "/report", element: <ReportPage /> },
+        { path: "/profile", element: <EmployeeProfile /> },
       ],
     },
-    {
-      path: "/signin",
-      element: <SignIn />,
-    },
-    {
-      path: "*",
-      element: <LandingPage />,
-    },
+    { path: "/signin", element: <SignIn /> },
+    { path: "*", element: <LandingPage /> },
   ]);
 
   return (
-    <>
+    <Provider store={store}>
       <ThemeProvider theme={themeMode === "light" ? theme.light : theme.dark}>
-        <GlobalStyle />
-        <DrawerWidthContext.Provider value={{ drawerWidth }}>
-          <RouterProvider router={router} />
-        </DrawerWidthContext.Provider>
+        <SnackbarProvider maxSnack={3}>
+          <GlobalStyle />
+          <DrawerWidthContext.Provider value={{ drawerWidth }}>
+            <RouterProvider router={router} />
+          </DrawerWidthContext.Provider>
+        </SnackbarProvider>
       </ThemeProvider>
-    </>
+    </Provider>
   );
 };
 
